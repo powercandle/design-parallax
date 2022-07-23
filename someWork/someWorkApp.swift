a98b9f68491536bcc1e2b6d97e453e9273f46efb
alex_ezhovv <aezhov4@gmail.com>
2022-07-23 17:47:38 +0800

    Initial Commit

---
+++ someWork/someWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  someWorkApp.swift
+//  someWork
+//
+//  Created by Артём Рябенко on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct someWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
